94400011dcd4fcaf7168120abd2a6ede32a4e41e
Sruthi Sivakumar <51859127+sruthi-hub@users.noreply.github.com>
2021-03-04 13:09:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats, main_app # import your app modules here
+from apps import about, main_app # import your app modules here
 
 app = MultiApp()
 
 # Add all your application here
-app.add_app("Home", home.app)
-app.add_app("Data Stats", data_stats.app)
+app.add_app("About", about.app)
 app.add_app("Network entropy calculator", main_app.app)
 
 # The main app
