a9a5b0a2e007bbfc2cc52ba1ba6fd67d3c14928a
Sruthi Sivakumar <51859127+sruthi-hub@users.noreply.github.com>
2021-03-04 13:07:06 -0500

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,18 +1,20 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from data.create_data import create_table
 
 def app():
-    st.title('Home')
+    st.title('About')
 
-    st.write("This is a sample home page in the mutliapp.")
-    st.write("See `apps/home.py` to know how to use it.")
+    st.write("This webapp allows researchers to calculate network entropy from any omic-based dataset.")
+    st.write("This is published as a part of the manuscript: The biphasic and age-dependent impact of Klotho on hallmarks of aging and skeletal muscle function")
+    st.text("")
+    st.write("Preprocessing includes generation of protein-protein interaction network from raw count matrix.")
+    st.write("Example code for preprocessing is available on Github: `sruthi-hub/sarcopenia-network-entropy`")
+    st.text("")
+    st.write("We acknowledge Menichetti. et. al. for developing the mathematical basis for network entropy calculation.")
+    st.write("Menichetti, G., Bianconi, G., Castellani, G., Giampieri, E., & Remondini, D. (2015). Multiscale characterization of ageing and cancer progression by a novel network entropy measure. Molecular BioSystems, 11(7), 1824-1831.")
+    st.text("")
+    st.text("")
 
-    st.markdown("### Sample Data")
-    df = create_table()
-    st.write(df)
 
-    st.write('Navigate to `Data Stats` page to visualize the data')
+    st.write('Navigate to `Network entropy calculator` page to compute network entropy from your data')
 
 
